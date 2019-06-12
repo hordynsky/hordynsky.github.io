ae8af5620e96c55d4e4dedbd6a4ed57e250fb465
a_hordynsky <andriihordynsky@gmail.com>
2019-06-12 13:33:02 +0300

    new header look

--- askaway/main.js
+++ askaway/main.js
@@ -153,3 +153,4 @@ let jumpToAnchor = (anchor) => {
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
     document.querySelector('.offer-card__count--empty').display = 'none';
 }
+
