ada6dd3d04d6fb3b5b166cab1e4e3df31ff782d2
a_hordynsky <andriihordynsky@gmail.com>
2019-06-27 17:52:12 +0300

    Added SingleUser too, left configuring of address with google-maps-react

--- selecto/src/components/SingleUser/index.js
+++ selecto/src/components/SingleUser/index.js
@@ -7,7 +7,8 @@ class SingleUser extends React.Component {
 
     state = {
         userId: 0,
-        user: []
+        user: [],
+        error: null
     }
 
     getUser = async userId => {
@@ -15,13 +16,12 @@ class SingleUser extends React.Component {
             const results = await axios.get(`${DATA_URLS.USER(userId)}`);
             const user = results.data;
 
-            console.log(results);
             this.setState({
                 user
             })
             
         } catch (error) {
-            console.error(error);
+            this.setState({error})
         }
     }
 
@@ -34,12 +34,32 @@ class SingleUser extends React.Component {
 
     render(){
         const {match,history} = this.props;
+        const {address, company, email, name, phone, username, website} = this.state.user;
+
         return(
-            <S.Post>
+            !this.state.error ? <S.User>
                 <S.GlobalStyle/>
+
+                <h1>{name}</h1>
+                <S.UserDetails>
+                    <S.UserImage>
+                        <img src="https://cdn0.iconfinder.com/data/icons/finance-icons-rounded/110/Office-Worker-512.png"/>
+                    </S.UserImage>
+                    <S.UserInformation>
+                        <li><span>Email</span>: <a href={`mailto:${email}`}>{email}</a></li>
+                        <li><span>Username</span>: {username}</li>
+                        <li><span>Website</span>: <a href={`http://${website}`} target="_blank">{website}</a></li>
+                        <li><span>Phone</span>: <a href={`tel:+${phone}`}>{phone}</a></li>
+                        <li><span>Company</span>: {company && company.name}</li>
+                    </S.UserInformation>
+                </S.UserDetails>
+
+                <S.UserAddress>
+                
+                </S.UserAddress>
+
                 <S.Button onClick={() => history.goBack()} style={{position: 'absolute',  bottom: '2rem', left: '1rem'}}>Return back</S.Button>
-                <h2>Hello from user: {match.params.userId}</h2>
-            </S.Post>
+            </S.User> : `Sorry, there is no user with id:${match.params.userId}`
         )
     }
 }
